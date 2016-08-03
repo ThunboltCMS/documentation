ed91c898bd32a116b8dbc56ce1c11a08b4db60da
MartkCz <antik494@gmail.com>
2016-08-03 21:55:06 +0200

    Fixed loader

--- src/loader.php
+++ src/loader.php
@@ -1,6 +1,6 @@
 <?php
 
-class ThunbolDocsLoader {
+class ThunboltDocsLoader {
 
 	public static function getFile(string $lang, string $file): string {
 		return __DIR__ . '/' . $lang . '/' . $file;
